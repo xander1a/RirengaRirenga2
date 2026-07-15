53e660bc1ad8dc656909d23783078aa60c7923a7
Xi-Tek <dushimimanaalexandre1@gmail.com>
2026-07-15 22:22:23 +0200

    hello me

--- app/Http/Controllers/CustomerPortalController.php
+++ app/Http/Controllers/CustomerPortalController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class CustomerPortalController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function dashboard()
     {
         $user     = auth()->user();
